0ce5a55479dc19df8097b40cb63e2e3b5c109cd4
mazi-kunle <olakunlemichael644@gmail.com>
2022-06-11 09:29:27 -0700

    3rd task

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -12,12 +13,13 @@
 int main(int argc, char **argv)
 {
 	char buffer[32];
-	char *b = buffer;
+	char *b, *token;
+	char path[32];
+	b = buffer;
 	size_t characters, bufsize = 32;
 	pid_t child_pid;
 	char *args[10] = {NULL};
 
-	args[0] = buffer;
 	while (1)
 	{
 		child_pid = fork();
@@ -31,6 +33,18 @@ int main(int argc, char **argv)
 			printf("#cisfun$ ");
 			characters = getline(&b, &bufsize, stdin);
 			buffer[characters - 1] = '\0';
+			token = strtok(buffer, " ");
+			if (*token == '/')
+			{
+				strcpy(path, token);
+			}else
+			{
+				strcpy(path, "/usr/bin/");
+				strcat(path, token);
+			}
+			args[0] = path;
+			args[1] = strtok(NULL, " ");
+			args[2] = strtok(NULL, " ");
 			if (execve(args[0], args, NULL) == -1)
 			{
 				printf("%s: No such file or directory\n", argv[0]);
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdlib.h>
+int argsplit(char *str, char **argv);
+
+#endif

38f218a1d7f4d4956ccca11074c18bb8b77f885d
Samuel <awosamuel@gmail.com>
2022-06-14 04:19:12 -0700

    shell

---
+++ _strlen.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ *
+ * @s: A pointer to the characters string.
+ * Return: The length of the character string.
+ */
+
+int _strlen(const char *s)
+{
+	int length = 0;
+	if (!s)
+		return (length);
+	for (length = 0; s[length]; length++)
+		;
+	return (length);
+
+}
--- shell.h
+++ shell.h
@@ -11,7 +11,15 @@ extern char **environ;
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
 
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
 char *path_search(char *cmd);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
